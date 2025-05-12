1898fb2ad21a64345b3feedcb444cfa9de14c58b
kaonashie <chenardkone@gmail.com>
2025-05-12 08:50:24 -0400

    init

---
+++ A2/main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by soma on 12/05/25.
+//
+
+#include "main.h"
+
+int main() {
+    return 0;
+}
---
+++ A6/main.h
@@ -0,0 +1,8 @@
+//
+// Created by soma on 12/05/25.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+#include <iostream>
+#endif //MAIN_H
